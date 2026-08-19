b9a5371f91f67e969ef032b2c1ae248c34427a18
foreown <65128211+foreown@users.noreply.github.com>
2026-08-19 14:39:56 +0200

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: Denne filen genereres av Next.js og skal ikke redigeres manuelt.
-// Se https://nextjs.org/docs/app/api-reference/config/typescript
